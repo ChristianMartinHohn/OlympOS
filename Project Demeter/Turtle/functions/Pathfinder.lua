786d2fb4ddfe5944cc75fe5b2f1a995bb7f51c5a
Christian <christian@hohn.net>
2025-04-08 00:31:05 +0200

    untested Pathfinder tweaking
    hab den shit noch nicht getestet aber das sollte sinn machen...
    
    hab den vorgang in #3 beschrieben

--- Project Demeter/Turtle/functions/Pathfinder.lua
+++ Project Demeter/Turtle/functions/Pathfinder.lua
@@ -7,6 +7,7 @@ Pathfinder.new = function()
     local function check_waypoint_file_exists()
         local file = io.open("/Data/WayPoints.txt", "r")
         if file then
+            file:close()
             return true
         else
             return false
@@ -15,10 +16,12 @@ Pathfinder.new = function()
 
     local function setup_waypoint_file()
         if check_waypoint_file_exists() then
-            return false
+            return true
         else
             local file = io.open("/Data/WayPoints.txt", "w")
             if file then
+                file:write(Base_Position)
+                file:close()
                 return true
             else
                 return false
@@ -26,22 +29,46 @@ Pathfinder.new = function()
         end
     end
 
-    local function write_waypoint_file() --Hier wird die Mission in eine Datei geschrieben wichtig ist das eine Table als input genommen wird
+    local function write_waypoint_file()
         local file = io.open("/Data/WayPoints.txt", "w")
         if file then
             file:write(textutils.serialize(waypoint_list))
             file:close()
         end
     end
-    
-    --irgendwo muss noch bei einem neustart die waypoint_list geladen werden und die waypoint file gelesen
+
+    local function check_waypoint_system()
+        if waypoint_list[1] == nil then
+        --wenn die waypoint_list leer ist, dann wurd die Turtle gerade erst gestartet
+            if check_waypoint_file_exists() then
+            --wenn eine waypoint file existiert, dann muss die turtle vor dem start sich schon mal bewegt haben
+                local file = io.open("/Data/WayPoints.txt", "r")
+                if file then
+                    waypoint_list = textutils.unserialize(file:read("*a"))
+                    file:close()
+                end
+            else
+            --wenn die waypoint file nicht existiert, dann hat die turtle sich noch nicht bewegt und steht an der Base_Position
+                if setup_waypoint_file() then
+                    waypoint_list = {}
+                    local file = io.open("/Data/WayPoints.txt", "r")
+                    if file then
+                        waypoint_list = textutils.unserialize(file:read("*a"))
+                        file:close()
+                    end
+                end
+            end
+        --wenn die waypoint_list nicht leer ist, dann ist die Turtle mitten im Programm und es muss nichts gemacht werden
+        end
+    end
 
     local function add_waypoint(direction)
         --problem ist das der Waypoint beim ersten schritt erstellt wird und dann hier gecheckt wird nach dem block auf dem gestartet wurde
         --Evtl ein Dev problem das gelöst wird durch Base_Position mit speichern, hier muss wahrscheinlich ein anderer Test geschrieben werden
-        setup_waypoint_file()
+        check_waypoint_system()
         local x, y, z = gps.locate()
-        if waypoint_list[#waypoint_list] == nil then
+        if waypoint_list[2] == nil then
+        --waypoint_list[2] wird gecheckt, da waypoint_list[1] immer die Base_Position ist
             local waypoint = {
                 Direction = direction,
                 GPS = {["x"]= x, ["y"] = y, ["z"] = z}
